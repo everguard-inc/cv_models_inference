04ef9f80da3651b488f9afe0cf22c9f29214e62f
RodionBukhanevych11 <rodionbukhanevych@gmail.com>
2023-03-29 14:21:03 +0300

    removed yolov5 submodule

--- src/yolov5/output/run_evaluation.sh
+++
@@ -1,6 +0,0 @@
-python evaluate.py \
---val_ann_coco /path/to/val.json \
---images_dir /path/to/images \
---config_path config.yaml \
---iou_threshold 0.5 \
---score_threshold 0.5
--- src/yolov5/run_train.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python train.py \
---img 736 --batch 4 --epochs 3 \
---data data/demo.yaml \
---weights yolov5m.pt --workers 4
