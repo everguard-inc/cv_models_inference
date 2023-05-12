841bccb8545088dbcedb9dfda6882a325dea835d
rodionbukhanevych11 <rodionbukhanevych@gmail.com>
2023-05-12 16:26:18 +0300

    update setup file

--- setup.py
+++ setup.py
@@ -6,10 +6,11 @@ with open("requirements.txt") as file:
 
 
 setup(
-    name="src",
+    name="cv_models_inference",
     description="Package for inference cv-models",
     packages=find_packages(),
     install_requires=requirements,
+    package_dir = {"": "src"},
     python_requires=">=3.8",
     setup_requires=["setuptools_scm"],
     use_scm_version={"version_scheme": "python-simplified-semver"},
